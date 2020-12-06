62aa2b2caf3b8b2c47eaf766f8c4a40991f119c4
pandabrand <pandabrand@gmail.com>
2020-12-06 17:48:27 -0600

    child theme

---
+++ web/app/themes/twentytwenty-child/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+  add_action('wp_enqueue_scripts', 'tt_child_enqueue_parent_styles');
+
+  function tt_child_enqueue_parent_styles()
+  {
+    wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
+    wp_enqueue_style('flickity-style', 'https://unpkg.com/flickity@2/dist/flickity.min.css');
+    wp_enqueue_script('flickity-script', 'https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', array(), null);
+  }
---
+++ web/app/themes/twentytwenty-child/template-parts/content.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both singular and index.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<?php
+
+	get_template_part( 'template-parts/entry-header' );
+
+	if ( ! is_search() ) {
+		get_template_part( 'template-parts/featured-image' );
+	}
+
+	?>
+
+	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
+
+		<div class="entry-content">
+
+			<?php
+			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
+				the_excerpt();
+			} else {
+				the_content( __( 'Continue reading', 'twentytwenty' ) );
+			}
+			?>
+      <div class="dates">
+        <?php 
+          $paint_dates = get_field('paint_dates');
+          foreach ($paint_dates as $key => $paint_date) :
+            $date = $paint_date['date'];
+            $location = $paint_date['location'];
+            $images = $paint_date['images'];
+        ?>
+          <div class="info">
+            <p class="date"><?php echo $date; ?></p>
+            <p class="location"><?php echo $location[0]->name; ?></p>
+          </div>
+          <div class="carousel" data-flickity='{"wrapAround": true }'>
+            <?php foreach ($images as $image) : ?>
+              <div class="image"><?php echo wp_get_attachment_image($image, 'large'); ?></div>
+            <?php endforeach; ?>
+          </div>
+        <?php
+          endforeach;
+        ?>
+        </div>
+		</div><!-- .entry-content -->
+
+	</div><!-- .post-inner -->
+
+	<div class="section-inner">
+		<?php
+		wp_link_pages(
+			array(
+				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+				'after'       => '</nav>',
+				'link_before' => '<span class="page-number">',
+				'link_after'  => '</span>',
+			)
+		);
+
+		edit_post_link();
+
+		?>
+
+	</div><!-- .section-inner -->
+
+	<?php
+
+	if ( is_single() ) {
+
+		get_template_part( 'template-parts/navigation' );
+
+	}
+?>
+
+</article><!-- .post -->
