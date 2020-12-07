c6fc3cd142ef444b1063d5c5631a1e20172f3aeb
pandabrand <pandabrand@gmail.com>
2020-12-06 18:12:11 -0600

    oops

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,3 +1,15 @@
 <?php
+/**
+ * Your base production configuration goes in this file. Environment-specific
+ * overrides go in their respective config/environments/{{WP_ENV}}.php file.
+ *
+ * A good default policy is to deviate from the production config as little as
+ * possible. Try to define as much of your configuration in this file as you
+ * can.
+ */
+
+use Roots\WPConfig\Config;
+use function Env\env;
+
 Config::define('WP_DEBUG', true);
 Config::define('WP_DEBUG_LOG', true);
